fbf160ae0b333c8b3bbe8e710087a5d212fb9f17
Selim Ibarra <selim.ibarra@gmail.com>
2015-02-08 23:01:12 -0800

    Added tasks to users and embedded user auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209033930) do
+ActiveRecord::Schema.define(version: 20150209063222) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
