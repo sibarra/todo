14f483883af21342d60f51177a0babdf7660e56c
Selim Ibarra <selim.ibarra@gmail.com>
2015-02-10 01:48:19 -0800

    Changed tasks view and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Stukdo::Application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
+  get 'test'  => 'pages#test'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
